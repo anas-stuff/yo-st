869854894c8ca81b030d7eeaa3509b995cba1382
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-08-05 17:47:07 +0200

    Change the background color to #1d1f21 💙

--- config.h
+++ config.h
@@ -121,7 +121,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#1d1f21", /* default background colour */
 };
 
 
